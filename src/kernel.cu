2d2a36815d05abf6fb55aa680fd8233939c535eb
HAN YAN <hyan99@park.key.seas.upenn.edu>
2020-09-09 15:46:09 -0400

    2.1 checkpoint1

--- src/kernel.cu
+++ src/kernel.cu
@@ -169,6 +169,12 @@ void Boids::initSimulation(int N) {
   gridMinimum.z -= halfGridWidth;
 
   // TODO-2.1 TODO-2.3 - Allocate additional buffers here.
+	cudaMalloc((void**)&dev_particleArrayIndices, N * sizeof(int));
+	checkCUDAErrorWithLine("cudaMalloc dev_particleArrayIndices failed!");
+
+	cudaMalloc((void**)&dev_particleGridIndices, N * sizeof(int));
+	checkCUDAErrorWithLine("cudaMalloc dev_particleGridIndices failed!");
+
   cudaDeviceSynchronize();
 }
 
@@ -332,6 +338,12 @@ __global__ void kernComputeIndices(int N, int gridResolution,
     // - Label each boid with the index of its grid cell.
     // - Set up a parallel array of integer indices as pointers to the actual
     //   boid data in pos and vel1/vel2
+	for (int i = 0; i < N; i++) {
+		glm::vec3 offset = glm::floor((pos[i] - gridMin) * inverseCellWidth);
+		int gridIndex = gridIndex3Dto1D(offset.x, offset.y, offset.z, gridResolution);
+		indices[i] = i;
+		gridIndices[i] = gridIndex;
+	}
 }
 
 // LOOK-2.1 Consider how this could be useful for indicating that a cell
@@ -349,6 +361,16 @@ __global__ void kernIdentifyCellStartEnd(int N, int *particleGridIndices,
   // Identify the start point of each cell in the gridIndices array.
   // This is basically a parallel unrolling of a loop that goes
   // "this index doesn't match the one before it, must be a new cell!"
+	int prev = -1;
+	for (int i = 0; i < N; i++) {
+		if (particleGridIndices[i] != prev) {
+			if (prev != -1) {
+				gridCellEndIndices[prev] = i - 1;
+			}
+			prev = particleGridIndices[i];
+			gridCellStartIndices[prev] = i;
+		}
+	}
 }
 
 __global__ void kernUpdateVelNeighborSearchScattered(
@@ -439,6 +461,8 @@ void Boids::endSimulation() {
   cudaFree(dev_pos);
 
   // TODO-2.1 TODO-2.3 - Free any additional buffers here.
+	cudaFree(dev_particleArrayIndices);
+	cudaFree(dev_particleGridIndices);
 }
 
 void Boids::unitTest() {
